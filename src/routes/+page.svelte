e5c522fed113a4a2083f6db02a9d2a5a968a4898
Nafis Faiyaz <nafis.faiyaz.5@gmail.com>
2025-09-06 13:23:02 +0600

    Updated about page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,31 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+</script>
+
+<div class="m-8 ">
+  <h1 class="text-4xl font-bold">Nafis Faiyaz</h1>
+  <p class="my-2">Hello, mysterious wanderer! Curious to know what brought you to my website. Anyway, I am currently
+    working as a software engineer
+    in <a
+      class="text-blue-500 hover:text-blue-900 visited:text-purple-600"
+      href="https://therapbd.com/">Therap BD</a>.
+    Here I work with Java Spring Boot. Some of my day-to-day responsibilities involve fiddling through undocumented
+    legacy codebase, resolving issues, fixing data inconsistencies, removing redundant code, enhancing maintainability
+    and performance.
+  </p>
+
+  <p class="my-2"> In a previous life, I was at <a
+    class="text-blue-500 hover:text-blue-900 visited:text-purple-600"
+    href="https://cse.iutoic-dhaka.edu/">IUT</a>, where I got my undergraduate degree, and secured <a
+    class="underline"
+    href="https://icpc.global/ICPCID/J5RB60S8U1J5">8th
+    place in 2022 ICPC Dhaka Regional</a> among many other achievements.
+  </p>
+
+  <p class="my-2">Recently I have been learning Rust and Svelte outside of my work hours. I am also interested in
+    Devops, Reinforcement Learning and Robotics.
+  </p>
+
+  <p class="my-2">{'{'}Insert a fun game}</p>
+
+  <!--  <p>Last updated: September, 2025</p>-->
+</div>
