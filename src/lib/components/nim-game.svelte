110a8a8b972b956128aa23df58df3f55393029b6
Nafis Faiyaz <nafis.faiyaz.5@gmail.com>
2025-09-07 19:34:48 +0600

    Added nim game

---
+++ src/lib/components/nim-game.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import { Input } from '$lib/components/ui/input/index.js';
+  import { Checkbox } from '$lib/components/ui/checkbox/index.js';
+
+  let coins = $state(20);
+  let coinsPerTurn = $state(2);
+  let goFirst = $state(false);
+  //
+  // function takeCoins(num: number) {
+  //   coins.set(coins() - num);
+  // }
+
+</script>
+
+<div class="m-8 border-2 p-2">
+  <p class="my-2">
+    <span class="font-bold"> Rules: </span> There is a pre-defined number of coins at the start of the game. Also, each
+    player on their turn can take any number of coins from 1 to n (also pre-defined). Whichever player takes the last
+    coin wins the game.
+  </p>
+
+  <p class="my-2">
+    <span class="font-bold"> Configuration: </span>
+    <br />
+    Total Number of coins: <Input class="inline max-w-3xs h-fit" type="number" bind:value="{coins}" />
+    <br />
+    Maximum Number of coins per turn: <Input class="inline max-w-3xs" type="number" bind:value="{coinsPerTurn}" />
+    <br />
+    Do you want to go first?
+    <Checkbox class="inline max-w-3xs" bind:checked={goFirst} />
+  </p>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,31 +1,38 @@
 <script>
+  import NimGame from '$lib/components/nim-game.svelte';
 </script>
 
 <div class="m-8 ">
   <h1 class="text-4xl font-bold">Nafis Faiyaz</h1>
-  <p class="my-2">Hello, mysterious wanderer! Curious to know what brought you to my website. Anyway, I am currently
-    working as a software engineer
-    in <a
-      class="text-blue-500 hover:text-blue-900 visited:text-purple-600"
-      href="https://therapbd.com/">Therap BD</a>.
-    Here I work with Java Spring Boot. Some of my day-to-day responsibilities involve fiddling through undocumented
-    legacy codebase, resolving issues, fixing data inconsistencies, removing redundant code, enhancing maintainability
-    and performance.
+  <p class="my-2">
+    Hello, mysterious wanderer! Curious to know what brought you to my website. Anyway, I am currently
+    working as a software engineer in <a class="text-blue-500 hover:underline" href="https://therapbd.com/">Therap
+    BD</a>. Here I work with Java, Spring Boot. Some of my day-to-day responsibilities involve fiddling through
+    undocumented legacy codebase, resolving issues, fixing data inconsistencies, removing redundant code, enhancing
+    maintainability and performance.
   </p>
 
-  <p class="my-2"> In a previous life, I was at <a
-    class="text-blue-500 hover:text-blue-900 visited:text-purple-600"
-    href="https://cse.iutoic-dhaka.edu/">IUT</a>, where I got my undergraduate degree, and secured <a
-    class="underline"
-    href="https://icpc.global/ICPCID/J5RB60S8U1J5">8th
-    place in 2022 ICPC Dhaka Regional</a> among many other achievements.
+  <p class="my-2">
+    Outside of my work hours, I have been learning Rust and Svelte. I am also interested in Devops, Reinforcement
+    Learning and Robotics.
   </p>
 
-  <p class="my-2">Recently I have been learning Rust and Svelte outside of my work hours. I am also interested in
-    Devops, Reinforcement Learning and Robotics.
+  <p class="my-2">
+    In a previous life, I was at <a class="text-blue-500 hover:underline" href="https://cse.iutoic-dhaka.edu/"> IUT </a>,
+    where I got my undergraduate degree, and secured <a class="text-blue-500 hover:underline"
+                                                        href="https://icpc.global/ICPCID/J5RB60S8U1J5"> 8th place in
+    2022 ICPC Dhaka Regional </a> among many other achievements. I was also among the top 20 National campers from
+    Bangladesh
+    Mathematical Olympiad in 2016.
   </p>
 
-  <p class="my-2">{'{'}Insert a fun game}</p>
+  <p class="my-2">
+    Here is a fun Nim game from my favorite <a class="text-blue-500 hover:underline"
+                                               href="https://www.amazon.com/Course-Game-Theory-Thomas-Ferguson/dp/9813227346">
+    game theory book </a> that I play with my friends and family to prank them. <span class="italic">(Spoilers: You will always lose)</span>
+  </p>
 
   <!--  <p>Last updated: September, 2025</p>-->
 </div>
+
+<NimGame />
